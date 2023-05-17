016a717c9e5aa3c847ac736ccf18a7c88ab7b108
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-05-17 16:49:32 +0300

    priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- -- Displays all privileges of the given MySQL user
--- SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1' @'localhost';
+
+SHOW GRANTS FOR 'user_0d_2' @'localhost';
